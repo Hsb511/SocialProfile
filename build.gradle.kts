2ce3a6ee062657e41906bec7c1397d08243a1b30
Hsb511 <hugo-selle@hotmail.fr>
2022-07-20 15:02:30 +0200

    [UPD] Improving gradle files with kts and buildSrc

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version Versions.ANDROID_PLUGIN apply false
+    id("com.android.library") version Versions.ANDROID_PLUGIN apply false
+    id("org.jetbrains.kotlin.android") version Versions.KOTLIN apply false
+}
+
+tasks {
+    val clean by registering(Delete::class) {
+        delete(rootProject.buildDir)
+    }
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,4 @@
+object Versions {
+    const val ANDROID_PLUGIN = "7.1.3"
+    const val KOTLIN = "1.6.20"
+}
