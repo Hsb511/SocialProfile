ab9498ae3dbddd2cd29c125d155793635856e075
Hsb511 <hugo-selle@hotmail.fr>
2022-07-31 21:49:04 +0200

    [REF] Cleaning up MainActivity

--- app/src/main/java/com/team23/socialprofile/MainActivity.kt
+++ app/src/main/java/com/team23/socialprofile/MainActivity.kt
@@ -1,22 +1,7 @@
 package com.team23.socialprofile
 
-import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.commit
-import com.team23.home.ui.fragments.HomeFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity: FragmentActivity(R.layout.activity_main) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-
-        /*if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true).add(com.team23.home.R.id.fragment_home, HomeFragment())
-            }
-        }*/
-    }
-}
+class MainActivity: FragmentActivity(R.layout.activity_main)
