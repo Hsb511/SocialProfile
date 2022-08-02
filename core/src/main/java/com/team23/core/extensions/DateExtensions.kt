53b874a6e200dbc51b650810f9543c7803109e58
Hsb511 <hugo-selle@hotmail.fr>
2022-08-02 14:01:35 +0200

    [UPD] Implementing the toDuration() method

--- core/src/main/java/com/team23/core/extensions/DateExtensions.kt
+++ core/src/main/java/com/team23/core/extensions/DateExtensions.kt
@@ -2,6 +2,7 @@ package com.team23.core.extensions
 
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 const val ISO_8601_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
 const val READABLE_DATE_TIME_FORMAT = "dd MMM yyyy HH:mm:ss"
@@ -18,5 +19,27 @@ fun String.fromISO8601toReadableDateTime() = this.fromISO8601().toString(READABL
 
 fun LocalDateTime.toBirthDate() = this.toString(BIRTHDATE_FORMAT)
 
-// TODO
-fun LocalDateTime.toDuration() = "13 j"
+fun LocalDateTime.toDuration(): String {
+    val yearDiff = ChronoUnit.YEARS.between(this, LocalDateTime.now())
+    if (yearDiff != 0L) {
+        return "$yearDiff a"
+    }
+    val monthsDiff = ChronoUnit.MONTHS.between(this, LocalDateTime.now())
+    if (monthsDiff != 0L) {
+        return "$monthsDiff m"
+    }
+    val dayDiff = ChronoUnit.DAYS.between(this, LocalDateTime.now())
+    if (dayDiff != 0L) {
+        return "$dayDiff j"
+    }
+    val hourDiff = ChronoUnit.HOURS.between(this, LocalDateTime.now())
+    if (hourDiff != 0L) {
+        return "$hourDiff h"
+    }
+    val minuteDiff = ChronoUnit.MINUTES.between(this, LocalDateTime.now())
+    if (minuteDiff != 0L) {
+        return "$minuteDiff min"
+    }
+    return "${ChronoUnit.SECONDS.between(this, LocalDateTime.now())} s"
+}
+
